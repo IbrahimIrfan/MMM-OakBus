c603c2e6b2dba15a19b5886a5aa5079208355e23
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-06-15 21:22:38 -0400

    Added working files

--- MMM-OakBus.js
+++ MMM-OakBus.js
@@ -1,24 +1,49 @@
-Module.register("MMM-OakBus",{
-	// Default module config.
-	defaults: {
+Module.register('MMM-OakBus', {
+
+    defaults: {
+            interval:   5000
+        },
+
+
+    start:  function() {
+        Log.log('Starting module: ' + this.name);
+
+        // Set up the local values, here we construct the request url to use
+        this.loaded = false;
+		this.url = "https://busfinder.oakvilletransit.ca/bustime/wireless/html/eta.jsp?route=1&direction=OAKVILLE+GO&id=3146&showAllBusses=on";
+        this.result = null;
+
+        // Trigger the first request
+        this.getAirQualityData(this);
+        },
+
+
+    getAirQualityData: function(that) {
+        that.sendSocketNotification('GET-AIR-QUALITY', that.url);
+        setTimeout(that.getAirQualityData, that.config.interval, that);
+        },
+
+    getDom: function() {
+        // Set up the local wrapper
+        var wrapper = null;
+        if (this.loaded) {
+			wrapper = document.createElement('div');
+			found = this.result.indexOf("<hr/>");
+			found2 = this.result.indexOf("<hr />");
+			wrapper.innerHTML = this.result.substr(found + 5, found2 - found);
+		} else {
+			wrapper = document.createElement('div');
+			wrapper.innerHTML = 'Loading bus route...';
+		}
+
+		return wrapper;
 	},
 
-	getScripts: function(){
- 		return [
-		'https://code.jquery.com/jquery-3.3.1.min.js'
-		]
-	}
-
-	// Override dom generator.
-	getDom: function() {
-		var wrapper = document.createElement("div");
-		var url = "https://busfinder.oakvilletransit.ca/bustime/wireless/html/eta.jsp?route=20&direction=OAKVILLE+GO&id=3045&showAllBusses=on";
-		
-		$.get(url, function(data){
-			var elements = $(data);
-			var found = $("body > strong:nth-child(18)", elements);
-			wrapper.innerHTML = found.val();
-			return wrapper;
-		});
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'GOT-AIR-QUALITY') {
+			this.loaded = true;
+			this.result = payload.result;
+			this.updateDom();
+		}
 	}
 });
--- node_helper.js
+++ node_helper.js
@@ -1,2 +1,32 @@
-var NodeHelper = require("node_helper");
-module.exports = NodeHelper.create({});
+var NodeHelper = require('node_helper');
+var request = require('request');
+
+module.exports = NodeHelper.create({
+
+    start: function () {
+        this.result = null;
+        },
+
+    getAirQualityData: function(payload) {
+        var that = this;
+        this.url = payload;
+
+        request({url: this.url, method: 'GET'}, function(error, response, body) {
+            // Check to see if we are error free and got an OK response
+            if (!error && response.statusCode == 200) {
+				that.result = body;
+			} 
+
+			// We have the response figured out so lets fire off the notifiction
+			that.sendSocketNotification('GOT-AIR-QUALITY', {'url': that.url, 'result': that.result});
+
+		});
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'GET-AIR-QUALITY') {
+			this.getAirQualityData(payload);
+		} 
+	}
+
+});
